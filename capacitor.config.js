0905e975963221766caf83929788cceebda67e66
SELVAelking <selvaselva111178@gmail.com>
2026-09-13 04:04:08 +0300

    Add files via upload

---
+++ capacitor.config.js
@@ -0,0 +1,11 @@
+const config = {
+  appId: 'com.selva.smartcalculator',
+  appName: 'Selva Smart Calculator',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+  android: {
+    backgroundColor: '#020617'
+  }
+};
+
+export default config;
